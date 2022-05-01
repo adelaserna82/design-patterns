067a9e35b0210e105ec18ae0352c1f27e3414c57
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-05-01 20:13:11 +0200

    Builder finiquitado
    Co-Authored-By: Jose Manuel Pinillos Rubio <75635580+JMPinillos@users.noreply.github.com>

--- src/Singleton/Client.java
+++ src/Singleton/Client.java
@@ -6,11 +6,13 @@ public class Client {
 
 	public static void main(String[] args) {
 		
-
+		//Aquí instanciamos a la base de datos peticionando una conexión SQL
 		Database myConexion = Database.getInstance("SQL");
-		
 		System.out.println(myConexion.getConexion());
-
+		
+		//Aquí volvemos a instanciar a la base de datos peticionando ACCES.
+		//El constructor detectará que ya esta instanciado y nos devolverá la primera conexión establecida.
+		Database myConexion2 = Database.getInstance("ACCES");
+		System.out.println(myConexion2.getConexion());
 	}
-
 }
--- src/Singleton/Database/Database.java
+++ src/Singleton/Database/Database.java
@@ -5,19 +5,16 @@ public class Database {
 	private static Database instance;
 	private String conexion;
 	
-	private static Database database(String c) {
-		
-		if (instance == null) {
-			instance = new Database();
-			instance.conexion = c;
-		}
-		
-		return instance;
+	private Database (String c) {
+		conexion = c;
 	}
 	
 	//Retornamos la instancia
 	public static Database getInstance(String c) {
-		database(c);
+		if (instance == null) {
+			instance = new Database(c);
+		}
+		return instance;
 	}
 
 	public String getConexion() {
